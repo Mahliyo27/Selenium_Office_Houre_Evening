9626a2ce6039022323cac504ceb3c77d048cd9a0
Mahliyo_27 <mahliyogafurova27@gmail.com>
2023-01-25 20:18:00 -0500

    add Tc001

---
+++ src/test/java/com/cydeo/tests/T001_FaceBookTitleCheck.java
@@ -0,0 +1,21 @@
+package com.cydeo.tests;
+
+public class T001_FaceBookTitleCheck {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }
+}
+/*
+TC #1: Facebook title verification
+1. Open Chrome browser
+2. Go to https://www.facebook.com
+3. Verify title:
+Expected: “Facebook – log in or sign up”
+
+ */

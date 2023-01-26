887ad27228cf98cad074b9858a1ce573687086a5
Mahliyo_27 <mahliyogafurova27@gmail.com>
2023-01-25 20:45:35 -0500

    add Tc001

--- src/test/java/com/cydeo/tests/T001_FaceBookTitleCheck.java
+++ src/test/java/com/cydeo/tests/T001_FaceBookTitleCheck.java
@@ -1,9 +1,43 @@
 package com.cydeo.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T001_FaceBookTitleCheck {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.facebook.com");
+
+       // WebElement facebook= driver.findElement(By.cssSelector(""));
+
+
+       // Expected: “Facebook – log in or sign up”
+        String expected ="Facebook - log in or sign up";
+
+
+
+
+        String actual = driver.getTitle();
+
+
+        if (expected.equals(actual)){
+            System.out.println("PASS!!!");
+        }else{
+            System.out.println("FAILED!!!");
+        }
+
+
+        Thread.sleep(5000);
+
+        driver.quit();
 
 
 
